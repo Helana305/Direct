aae532c9989a9091c70c3ab5abb4048256cbea99
Helana305 <cfhelana@gmail.com>
2025-11-22 13:41:20 +0530

    Add files via upload

---
+++ arithmetic.py
@@ -0,0 +1,11 @@
+c=3
+d=5
+print("floor division",c//d)
+print(5+4-2)
+print(5**2+5)
+a=20 
+b=30
+c=50
+print(a<b and a<c)
+print(a<b or b>a)
+print(not a<b)
---
+++ datatypes.py
@@ -0,0 +1,23 @@
+#tuple
+t=(2,4,6,8)
+print(t[2])
+#t[2]=3
+#print(t)
+
+#dictinory
+alpha={"a":1,"b":2 ,"c":6}
+#print(alpha[a])
+alpha['a']=5
+print(alpha)
+
+#set
+s=set((10,20,30,30,40,40))
+print(s)
+a=0
+print(not a)
+x=5
+print(x<<2)
+print(x<<1)
+print(x>>1)
+print(x>>2)
+print(x>>3)
